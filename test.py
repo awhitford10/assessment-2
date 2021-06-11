8960b3ae8f3c806b8b253a407d0cf76615965640
awhitford10 <awhitford@Andrews-Mac-mini.local>
2021-06-11 18:06:54 -0400

    Andrew_asses2_final

---
+++ test.py
@@ -0,0 +1,14 @@
+import unittest
+from classes.inventory import Inventory
+from interface import Interface
+
+
+class Assessment2Test(unittest.TestCase):
+
+    def test_view_inventory(self):
+        test1 = Interface()
+        self.assertEqual()
+
+
+if __name__=='__main__':
+    unittest.main()
